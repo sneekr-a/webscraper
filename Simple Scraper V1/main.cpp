85711cbe937f9a9473fe54feafe93c84c8425309
hcldev <hcldev@protonmail.com>
2020-12-07 20:26:21 -0600

    renamed folder for decreased egoistic naming behaviors

--- Simple Scraper V1/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "implementation.cpp"
-
-int main() {
-	const char url[] = "http://www.example.com/";
-	const char outfilename[] = "httpget.txt";
-
-	return getHTML(url, outfilename);
-}
---
+++ webscraper or something/implementation.cpp
@@ -0,0 +1,51 @@
+#include "curl/curl.h"
+#include <stdio.h>
+#include <string>
+
+
+/*
+Function getHTML takes a string url and writes the HTML code to a string outfilename, which is the name of the output file
+*/
+int getHTML(const char url[], const char outfilename[]) {
+
+	//--def and init
+	CURL* curl = curl_easy_init();
+	FILE* fp;
+
+	//--setup
+
+	//-open file
+	fp = fopen(outfilename, "w");
+	if (!fp) {
+		fprintf(stderr, "file open failed");
+		return EXIT_FAILURE;
+	}
+
+	//-set options
+	CURLcode code = CURLE_OK;
+
+	//set url option
+	if ((code = curl_easy_setopt(curl, CURLOPT_URL, url)) != CURLE_OK) {
+		fprintf(stderr, curl_easy_strerror(code));
+		return EXIT_FAILURE;
+	}
+
+	//set writefile option
+	if ((code = curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp)) != CURLE_OK) {
+		fprintf(stderr, curl_easy_strerror(code));
+		return EXIT_FAILURE;
+	}
+
+	//--perform the curl and pull html data
+	if ((code = curl_easy_perform(curl)) != CURLE_OK) {
+		fprintf(stderr, curl_easy_strerror(code));
+		return EXIT_FAILURE;
+	}
+
+
+	//--perform cleanup
+	curl_easy_cleanup(curl);
+	fclose(fp);
+
+	return EXIT_SUCCESS;
+}
